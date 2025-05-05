245d9360500a9fd9644237faebb2c55c928860ae
ThereseGlode <therese.glodedata@gmail.com>
2025-05-05 14:03:07 +0200

    slide

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('Hello!');
+document.addEventListener("DOMContentLoaded", function () {
+  const logo = document.getElementById("logo");
+  const container = document.getElementById("easteregg");
+
+  logo.addEventListener("click", function () {
+    container.classList.toggle("yellow-background");
+  });
+});
