294097517ec0f608e83bc001b5a958897fc79566
ThereseGlode <therese.glodedata@gmail.com>
2025-05-06 13:53:01 +0200

    sunny

--- script.js
+++ script.js
@@ -6,3 +6,22 @@ document.addEventListener("DOMContentLoaded", function () {
     container.classList.toggle("yellow-background");
   });
 });
+
+// Hent elementene
+const burgerIcon = document.getElementById('burger-icon');
+const headerNav = document.getElementById('headerNav');
+
+// Legg til en event listener for å vise/skjule menyen
+burgerIcon.addEventListener('click', () => {
+  // Legger til eller fjerner 'show' klassen på navigasjonen
+  headerNav.classList.toggle('show');
+});
+
+const logo2 = document.getElementById("logo2");
+const container2 = document.getElementById("easteregg2");
+
+if (logo2 && container2) {
+  logo2.addEventListener("click", function () {
+    container2.classList.toggle("yellow-background");
+  });
+}
